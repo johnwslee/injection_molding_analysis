9b312bee3525cb5ffba58959453c5c601af0d4b6
johnwslee <john.ws.lee@gmail.com>
2023-06-28 00:44:54 +0900

    Checked on the codes

---
+++ src/utils.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.model_selection import cross_validate
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+
+
+def mean_std_cross_val_scores(model, X_train, y_train, **kwargs):
+    """
+    Returns mean and std of cross validation
+
+    Parameters
+    ----------
+    model :
+        scikit-learn model
+    X_train : numpy array or pandas DataFrame
+        X in the training data
+    y_train :
+        y in the training data
+
+    Returns
+    ----------
+        pandas Series with mean scores from cross_validation
+    """
+
+    scores = cross_validate(model, X_train, y_train, **kwargs)
+
+    mean_scores = pd.DataFrame(scores).mean()
+    std_scores = pd.DataFrame(scores).std()
+    out_col = []
+
+    for i in range(len(mean_scores)):
+        out_col.append((f"%0.3f (+/- %0.3f)" % (mean_scores[i], std_scores[i])))
+
+    return pd.Series(data=out_col, index=mean_scores.index)
+
+
+def plot_roc_curve(true_y, y_prob):
+    """
+    plots the roc curve based of the probabilities
+    """
+    plt.figure(figsize=(3, 2.5))
+    fpr, tpr, thresholds = roc_curve(true_y, y_prob)
+    plt.plot(fpr, tpr)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.show();
